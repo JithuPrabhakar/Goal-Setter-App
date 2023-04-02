41bca428b74aa163a006710bdf5ce73a2cd437f3
Jithu Prabhakar <join.jitz@gmail.com>
2023-04-02 08:42:12 +0530

    Rest API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+
+const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
+
+connectDB()
 
 const port = process.env.PORT || 5000
 
